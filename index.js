82be37a154fd35f6553f3648dbbe7cb8b631be56
root <root@bitcoin-order-scrapper.c.my-gc-playground.internal>
2017-12-22 04:49:47 +0000

    added error logger

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('daemon')();
+require('daemon')();
 const fetchUrl = require('fetch').fetchUrl;
 const jsonfile = require('jsonfile');
 const exec = require('promised-exec');
@@ -15,23 +15,35 @@ const log_file = 'logs/log.txt'
 const file_name = ['BTC', 'LTC', 'ETH'];
 
 function myFun(ext, i) {
-
-  try{
-    fetchUrl(order_url[i], (err, res, data) => {
-      const JSON_object = JSON.parse(data.toString());
+  fetchUrl(order_url[i], (err, res, data) => {
+    if (err) {
+      fs.appendFile(log_file, 'failed to save at ' + ext + '\n');
+    } else {
+      const JSON_object  = JSON.parse(data.toString());
       const file = 'data/' + ext + '-' + file_name[i] + '.json';
       jsonfile.writeFile(file, JSON_object);
-    });
-  } catch(e) {
-    fs.appendFile(log_file, 'failed to save at ' + ext + '\n');
-  }
+    }
 
-    // exec('gsutil cp ' + file + ' gs://mingrui-bucket/bitcoin-book/' + file).then((res) => {
-    //   console.log(res)
-    //   exec('rm ' + file)
-    // })
-    // exec('gsutil cp ' + file + ' gs://mingrui-bucket/bitcoin-book/' + file)
-    // exec('rm ' + file)
+    // try {
+    //   const JSON_object = JSON.parse(data.toString());
+    //   const file = 'data/' + ext + '-' + file_name[i] + '.json';
+    //   jsonfile.writeFile(file, JSON_object);
+    // } catch(e) {
+    //   fs.appendFile(log_file, 'failed to save at ' + ext + '\n');
+    // }
+
+    // const JSON_object = JSON.parse('<dovtype HTML>');
+    // const JSON_object = JSON.parse(data.toString());
+    // const file = 'data/' + ext + '-' + file_name[i] + '.json';
+    // jsonfile.writeFile(file, JSON_object);
+  });
+
+  // exec('gsutil cp ' + file + ' gs://mingrui-bucket/bitcoin-book/' + file).then((res) => {
+  //   console.log(res)
+  //   exec('rm ' + file)
+  // })
+  // exec('gsutil cp ' + file + ' gs://mingrui-bucket/bitcoin-book/' + file)
+  // exec('rm ' + file)
 
 }
 
@@ -40,9 +52,9 @@ cur_time = cur_time - cur_time % 1000;
 let today = new Date(cur_time);
 let cur_date = today.getUTCDate();
 let ext = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate() + '/';
-mkdirp('data/' + ext + 'BTC/')
-mkdirp('data/' + ext + 'LTC/')
-mkdirp('data/' + ext + 'ETH/')
+mkdirp('data/' + ext + 'BTC/');
+mkdirp('data/' + ext + 'LTC/');
+mkdirp('data/' + ext + 'ETH/');
 
 for (let i=0; i<3; i++) {
   myFun(file_name[i] + '/' + ext + cur_time, i);
@@ -53,12 +65,12 @@ setInterval(() => {
   cur_time = cur_time - cur_time % 1000;
   today = new Date(cur_time);
   if (today.getUTCDate() != cur_date) {
-    cur_date = today.getUTCDate()
+    cur_date = today.getUTCDate();
     
     ext = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate() + '/';
-    mkdirp('data/' + ext + 'BTC/')
-    mkdirp('data/' + ext + 'LTC/')
-    mkdirp('data/' + ext + 'ETH/')
+    mkdirp('data/' + ext + 'BTC/');
+    mkdirp('data/' + ext + 'LTC/');
+    mkdirp('data/' + ext + 'ETH/');
   }
 
   for (let i=0; i<3; i++) {
