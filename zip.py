e75dc823ce6fca5ee41d4c92f71b8bb879a7531c
Wang Ming Rui <mingruimingrui@hotmail.com>
2018-03-09 00:24:19 +0800

    added zip.py

---
+++ zip.py
@@ -0,0 +1,19 @@
+import os
+import zipfile
+import datetime
+
+def zipdir(path, ziproot, ziph):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file), os.path.join(root[(len(path) + 1):], file))
+
+cur_date = datetime.datetime.utcnow()
+formatted_date = str(cur_date)[:10]
+data_path = 'data-science-bowl-2018'
+dates = [d for d in os.listdir(data_path) if d != formatted_date]
+
+for date in dates:
+    zipf = zipfile.ZipFile('data-zip/' + date + '.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipdir(os.path.join(data_path, date), '.', zipf)
+    zipf.close()
+    print('Successfully zipped', os.path.join(data_path, date))
